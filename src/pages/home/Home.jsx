1947ef8ca356c7097d2c18b1ea715c7704d6ace5
Tanvi Agrawal <tanviagrawal9913@gmail.com>
2026-06-22 15:43:16 +0530

    update resume

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -40,7 +40,7 @@ const Home = () => {
 
         <div className='buttons'>
           <a
-            href="https://drive.google.com/file/d/1F1J1vpRoN1Y6UofeKU8957fctrtCUACz/view?usp=sharing"
+            href="https://drive.google.com/file/d/1Dd0eYbAQEZJwz0yMKbRPKsQ8glS3pXzt/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className='download-resume'
